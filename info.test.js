50cf26965e307d27228e14819e6b84185d3867de
Vasily Shilov <vasily@aidriller.com>
2020-06-22 00:14:38 +0700

    210620-3

--- info.test.js
+++ info.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest')
 const agent = request('https://sigen.pro/v1')
 
-describe("Набор тестов 3", function() {
-    it("Тест 3.1", async function() {
+describe("api_sigen_info", function() {
+    it("info 3.1", async function() {
       await agent
         .get('/web-public/exchange/info')
         .expect('Content-Type', /json/)
