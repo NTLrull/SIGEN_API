2cfffc0f7cd5d92935a5c258c5e8cad31613f226
Shilov Vasily <shilov_vasily@smartworld.team>
2020-06-23 12:11:28 +0700

    23062020-1

--- info.test.js
+++ info.test.js
@@ -2,10 +2,23 @@ const request = require('supertest')
 const agent = request('https://sigen.pro/v1')
 
 describe("api_sigen_info", function() {
-    it("info 3.1", async function() {
-      await agent
-        .get('/web-public/exchange/info')
-        .expect('Content-Type', /json/)
-        .expect(200)
+    it("Info 3.1", async function() {
+      await new Promise(function(resolve, reject) {
+      return agent
+      .get('/web-public/exchange/info')
+      .expect('Content-Type', /json/)
+      .expect(function(res) {
+        expect(res.statusCode).toEqual(200)
+      })
+      .end(function(err, res){
+        if (err) {
+          reject(`\r\n
+          Ошибка: ${err.stack.replace(/[\"\\]/g, '')}\r\n\r\n
+          Данные: ${JSON.stringify(res, null, 2).replace(/[\"\\]/g, '')}`)
+        } else {
+          resolve(res)
+        }
+      })
     })
+  })
 })
